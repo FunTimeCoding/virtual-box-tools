bb1b1c315bcf6b70730a0cff3c4498fbae53fe47
Alexander Reitzel <funtimecoding@gmail.com>
2015-07-27 15:36:57 +0200

    add more scripts for listing, shares and forwardings

---
+++ bin/show-shares.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+usage(){
+    echo "Usage: ${0} VM_NAME"
+}
+
+if [ "${1}" = "" ]; then
+    usage
+    exit 1
+fi
+
+vboxmanage showvminfo "${1}" --details --machinereadable | grep SharedFolder
