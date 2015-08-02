dc5c007286cc7fe8a61ee0b0025d1ca89542b785
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-02 11:47:37 +0200

    add delete-vm command

---
+++ bin/delete-vm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+usage(){
+    echo "Usage: ${0} VM_NAME"
+}
+
+if [ "${1}" = "" ]; then
+    usage
+    exit 1
+fi
+
+if [ "${2}" = "" ]; then
+    usage
+    exit 1
+fi
+
+vboxmanage unregistervm "${1}" --delete
