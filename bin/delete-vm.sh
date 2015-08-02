c4b9d6b7e94fea2d4215d6d49346e19eb84f72f3
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-02 13:23:27 +0200

    extend delete-vm to also shutdown vm if its running

--- bin/delete-vm.sh
+++ bin/delete-vm.sh
@@ -10,4 +10,20 @@ if [ "${1}" = "" ]; then
     exit 1
 fi
 
+NOT_FOUND=false
+bin/show-vm-info.sh > /dev/null || NOT_FOUND=true
+
+if [ "${NOT_FOUND}" = true ]; then
+    echo "Not found."
+
+    exit 1
+else
+    IS_RUNNING=$(bin/list-vms.sh | grep "${1}")
+
+    if [ ! "${IS_RUNNING}" = "" ]; then
+        echo "Stop vm."
+        bin/stop-vm.sh "${1}"
+    fi
+fi
+
 vboxmanage unregistervm "${1}" --delete
---
+++ bin/show-vm-info.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+usage(){
+    echo "Usage: ${0} VM_NAME"
+}
+
+if [ "${1}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+vboxmanage showvminfo "${1}"
