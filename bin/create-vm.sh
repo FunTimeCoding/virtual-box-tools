c7c4223c8361ea81a45728c2a7e5260f6c3d6a14
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-02 14:06:48 +0200

    make create script also wait for VM to be up

--- bin/create-vm.sh
+++ bin/create-vm.sh
@@ -13,3 +13,16 @@ fi
 bin/clone-vm.sh jessie "${1}"
 bin/start-vm.sh "${1}"
 bin/get-vm-ip.sh "${1}"
+
+for SECOND in $(seq 1 120); do
+    echo "${SECOND}"
+    sleep 1
+    IP=$(bin/get-vm-ip.sh "${1}")
+
+    if [ ! "${IP}" = "" ]; then
+
+        break
+    fi
+done
+
+echo "${IP}"
