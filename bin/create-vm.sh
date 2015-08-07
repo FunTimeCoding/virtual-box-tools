328ef5d2feb099201423813a70778be3f5d9a741
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-07 22:14:14 +0200

    add argument loop and verbose argument to create-vm script

--- bin/create-vm.sh
+++ bin/create-vm.sh
@@ -3,15 +3,31 @@
 SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 
 usage(){
-    echo "Usage: ${0} [--wait] VM_NAME"
+    echo "Usage: ${0} [--help][--wait][--verbose] NAME"
 }
 
 WAIT=false
 
-if [ "${1}" = "--wait" ]; then
-    WAIT=true
-    shift
-fi
+while true; do
+    case ${1} in
+        --wait)
+            WAIT=true
+            shift
+            ;;
+        --help)
+            usage
+
+            exit 0
+            ;;
+        --verbose)
+            set -x
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 if [ "${1}" = "" ]; then
     usage
@@ -19,8 +35,10 @@ if [ "${1}" = "" ]; then
     exit 1
 fi
 
-"${SCRIPT_DIR}/clone-vm.sh" jessie "${1}"
-"${SCRIPT_DIR}/start-vm.sh" "${1}"
+NAME="${1}"
+
+"${SCRIPT_DIR}/clone-vm.sh" jessie "${NAME}"
+"${SCRIPT_DIR}/start-vm.sh" "${NAME}"
 
 if [ "${WAIT}" = "true" ]; then
     echo "Wait for IP."
@@ -28,7 +46,7 @@ if [ "${WAIT}" = "true" ]; then
 
     for SECOND in $(seq 1 60); do
         sleep 1
-        IP=$("${SCRIPT_DIR}/get-vm-ip.sh" "${1}")
+        IP=$("${SCRIPT_DIR}/get-vm-ip.sh" "${NAME}")
 
         if [ ! "${IP}" = "" ]; then
 
@@ -37,7 +55,7 @@ if [ "${WAIT}" = "true" ]; then
         fi
     done
 
-    MAC=$("${SCRIPT_DIR}/get-vm-mac.sh" --colons "${1}")
+    MAC=$("${SCRIPT_DIR}/get-vm-mac.sh" --colons "${NAME}")
     echo "BOOT_TIME: ${BOOT_TIME}"
     echo "IP: ${IP}"
     echo "MAC: ${MAC}"
