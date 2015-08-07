6657dc3d8181d201d0ce1490c6f23eada2f14777
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-07 21:28:15 +0200

    add wait argument to create-vm script, remove obsolete get mac colons script

--- bin/create-vm.sh
+++ bin/create-vm.sh
@@ -3,8 +3,15 @@
 SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 
 usage(){
-    echo "Usage: ${0} VM_NAME"
+    echo "Usage: ${0} [--wait] VM_NAME"
 }
+
+WAIT=false
+
+if [ "${1}" = "--wait" ]; then
+    WAIT=true
+    shift
+fi
 
 if [ "${1}" = "" ]; then
     usage
@@ -15,16 +22,22 @@ fi
 "${SCRIPT_DIR}/clone-vm.sh" jessie "${1}"
 "${SCRIPT_DIR}/start-vm.sh" "${1}"
 
-echo "Wait for IP."
-for SECOND in $(seq 1 60); do
-    sleep 1
-    IP=$("${SCRIPT_DIR}/get-vm-ip.sh" "${1}")
+if [ "${WAIT}" = "true" ]; then
+    echo "Wait for IP."
+
+    for SECOND in $(seq 1 60); do
+        sleep 1
+        IP=$("${SCRIPT_DIR}/get-vm-ip.sh" "${1}")
+
+        if [ ! "${IP}" = "" ]; then
+
+            break
+        fi
+    done
+
     MAC=$("${SCRIPT_DIR}/get-vm-mac.sh" --colons "${1}")
-
-    if [ ! "${IP}" = "" ]; then
-
-        break
-    fi
-done
-
-echo "IP: ${IP}"
+    echo "IP: ${IP}"
+    echo "MAC: ${MAC}"
+else
+    echo "VM is booting."
+fi
