72e8fcc78aa9984bb4d0ee326ad3b9e143b2fa14
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-07 20:45:37 +0200

    add script to get mac addresses

---
+++ bin/get-vm-mac.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+usage(){
+    echo "Usage: ${0} VM_NAME"
+}
+
+if [ "${1}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+KEY="MAC"
+VALUE=$(vboxmanage guestproperty enumerate "${1}" | grep "${KEY}")
+VALUE="${VALUE#*value: }"
+VALUE="${VALUE%%,*}"
+echo ${VALUE}
