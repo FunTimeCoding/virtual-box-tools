99c95d16703936b565bf0393ec75a7ecfa5bb344
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-07 22:25:40 +0200

    add verbose flag to delete script

--- bin/delete-vm.sh
+++ bin/delete-vm.sh
@@ -3,8 +3,13 @@
 SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 
 usage(){
-    echo "Usage: ${0} VM_NAME"
+    echo "Usage: ${0} [--verbose] VM_NAME"
 }
+
+if [ "${1}" = "--verbose" ]; then
+    set -x
+    shift
+fi
 
 if [ "${1}" = "" ]; then
     usage
@@ -12,25 +17,26 @@ if [ "${1}" = "" ]; then
     exit 1
 fi
 
+NAME="${1}"
 NOT_FOUND=false
-"${SCRIPT_DIR}/show-info.sh" "${1}" > /dev/null 2>&1 || NOT_FOUND=true
+"${SCRIPT_DIR}/show-info.sh" "${NAME}" > /dev/null 2>&1 || NOT_FOUND=true
 
 if [ "${NOT_FOUND}" = true ]; then
-    echo "Not found: ${1}"
+    echo "Not found: ${NAME}"
 
     exit 1
 else
-    IS_RUNNING=$("${SCRIPT_DIR}/list-vms.sh" | grep "${1}")
+    IS_RUNNING=$("${SCRIPT_DIR}/list-vms.sh" | grep "${NAME}")
 
     if [ ! "${IS_RUNNING}" = "" ]; then
         echo "Stop vm."
-        "${SCRIPT_DIR}/stop-vm.sh" "${1}"
+        "${SCRIPT_DIR}/stop-vm.sh" "${NAME}"
         DOWN=false
 
         for SECOND in $(seq 1 30); do
             echo "${SECOND}"
             sleep 1
-            STATE=$(vboxmanage showvminfo --machinereadable "${1}" | grep "VMState=")
+            STATE=$(vboxmanage showvminfo --machinereadable "${NAME}" | grep "VMState=")
             STATE=${STATE#*=}
             STATE=$(echo "${STATE}" | sed 's/"//g')
 
@@ -43,10 +49,10 @@ else
 
         if [ "${DOWN}" = "false" ]; then
             echo "Force shutdown."
-            "${SCRIPT_DIR}/stop-vm.sh" --force "${1}"
+            "${SCRIPT_DIR}/stop-vm.sh" --force "${NAME}"
             sleep 3
         fi
     fi
 fi
 
-vboxmanage unregistervm "${1}" --delete
+vboxmanage unregistervm "${NAME}" --delete
