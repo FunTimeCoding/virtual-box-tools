4901dcb30f596d8ec35b25fd27970eab0a0c4562
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-07 21:03:12 +0200

    add colons version of mac script

---
+++ bin/get-vm-mac-colons.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
+
+usage(){
+    echo "Usage: ${0} VM_NAME"
+}
+
+if [ "${1}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+VALUE=$("${SCRIPT_DIR}/get-vm-mac.sh" "${1}")
+LIST=$(echo "${VALUE}" | fold -w2)
+RESULT=""
+
+for HEX in ${LIST}; do
+    RESULT="${RESULT}:"
+done
+
+RESULT="${RESULT%:}"
+echo "${RESULT}"
