4d299ec3470a8c61da2451390f53cc9246602659
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-09 17:24:42 +0200

    add a setup.py to install this

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='virtualbox-tools',
+    version='0.1',
+    description='Stub description for virtualbox-tools.',
+    packages=['virtualbox_tools'],
+    author='Alexander Reitzel',
+    author_email='funtimecoding@gmail.com',
+    url='http://example.org',
+    download_url='http://example.org/virtualbox-tools.tar.gz',
+    install_requires=['pyyaml']
+)
