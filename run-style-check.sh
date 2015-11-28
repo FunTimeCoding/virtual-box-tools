efa228f54c0665d0260766ebc8d76d479a034872
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-28 13:24:40 +0100

    update style check script

--- run-style-check.sh
+++ run-style-check.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 
-find . -name '*.sh' -exec sh -c 'shellcheck ${1} || true' '_' '{}' \;
+if [ "$(command -v shellcheck || true)" = "" ]; then
+    echo "Command not found: shellcheck"
+
+    exit 1
+fi
+
+find . \( -name '*.sh' -and -not -path '*/.vim/*' \) -exec sh -c 'shellcheck ${1} || true' '_' '{}' \;
