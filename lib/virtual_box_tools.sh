48c9b9297f6f785b0c4a09b175b7874e24500b1f
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-28 16:16:19 +0100

    unwrap functions in library

--- lib/virtual_box_tools.sh
+++ lib/virtual_box_tools.sh
@@ -48,65 +48,50 @@ done
 
 OPTIND=1
 
-find_config()
-{
-    if [ "${VERBOSE}" = true ]; then
-        echo "find_config"
-    fi
+if [ "${VERBOSE}" = true ]; then
+    echo "find_config"
+fi
 
-    if [ "${CONFIG}" = "" ]; then
-        CONFIG="${HOME}/.virtual-box-tools.yml"
-    fi
+if [ "${CONFIG}" = "" ]; then
+    CONFIG="${HOME}/.virtual-box-tools.yml"
+fi
 
-    REALPATH_EXISTS=$(command -v realpath 2>&1)
+REALPATH_EXISTS=$(command -v realpath 2>&1)
+
+if [ ! "${REALPATH_EXISTS}" = "" ]; then
+    REALPATH_CMD="realpath"
+else
+    REALPATH_EXISTS=$(command -v grealpath 2>&1)
 
     if [ ! "${REALPATH_EXISTS}" = "" ]; then
-        REALPATH_CMD="realpath"
+        REALPATH_CMD="grealpath"
     else
-        REALPATH_EXISTS=$(command -v grealpath 2>&1)
+        echo "Required tool (g)realpath not found."
 
-        if [ ! "${REALPATH_EXISTS}" = "" ]; then
-            REALPATH_CMD="grealpath"
-        else
-            echo "Required tool (g)realpath not found."
-
-            exit 1
-        fi
+        exit 1
     fi
+fi
 
-    if [ -f "${CONFIG}" ]; then
-        CONFIG=$(${REALPATH_CMD} "${CONFIG}")
-    else
-        CONFIG=""
-    fi
-}
+if [ -f "${CONFIG}" ]; then
+    CONFIG=$(${REALPATH_CMD} "${CONFIG}")
+else
+    CONFIG=""
+fi
 
-find_config
+if [ "${VERBOSE}" = true ]; then
+    echo "load_config"
+fi
 
-load_config()
-{
-    if [ "${VERBOSE}" = true ]; then
-        echo "load_config"
-    fi
+if [ ! "${CONFIG}" = "" ]; then
+    SUDO_USER=$(shyaml get-value "sudo_user" < "${CONFIG}" 2>/dev/null || true)
+fi
 
-    if [ ! "${CONFIG}" = "" ]; then
-        SUDO_USER=$(shyaml get-value "sudo_user" < "${CONFIG}" 2>/dev/null || true)
-    fi
-}
+if [ "${VERBOSE}" = true ]; then
+    echo "define_library_variables"
+fi
 
-load_config
-
-define_library_variables()
-{
-    if [ "${VERBOSE}" = true ]; then
-        echo "define_library_variables"
-    fi
-
-    if [ ! "${SUDO_USER}" = "" ]; then
-        MANAGE_COMMAND="sudo -u ${SUDO_USER} vboxmanage"
-    else
-        MANAGE_COMMAND="vboxmanage"
-    fi
-}
-
-define_library_variables
+if [ ! "${SUDO_USER}" = "" ]; then
+    MANAGE_COMMAND="sudo -u ${SUDO_USER} vboxmanage"
+else
+    MANAGE_COMMAND="vboxmanage"
+fi
