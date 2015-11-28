2dcac1332e2becdcc51fd287bc9f7e51f1bc4e3e
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-28 15:56:09 +0100

    use manage command variable without quotes to allow spaces init

--- bin/list-vms.sh
+++ bin/list-vms.sh
@@ -3,4 +3,4 @@
 DIR=$(dirname "${0}")
 SCRIPT_DIR=$(cd "${DIR}" || exit 1; pwd)
 . "${SCRIPT_DIR}/../lib/virtual_box_tools.sh"
-"${MANAGE_COMMAND}" list runningvms | awk -F'"' '{ print $2 }'
+${MANAGE_COMMAND} list runningvms | awk -F'"' '{ print $2 }'
