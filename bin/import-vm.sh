3b4e4f25e7e0188214e2c415598dda49f68a7c2c
Alexander Reitzel <funtimecoding@gmail.com>
2016-05-15 22:49:40 +0200

    use sudo for a single command only

--- bin/import-vm.sh
+++ bin/import-vm.sh
@@ -1,9 +1,20 @@
 #!/bin/sh -e
 
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Usage: ${0} PATH_TO_VM"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}"/../lib/virtual_box_tools.sh
+
 PATH_TO_VM="${1}"
 
 if [ "${PATH_TO_VM}" = "" ]; then
-    echo "Usage: PATH_TO_VM"
+    usage
 
     exit 1
 fi
@@ -24,7 +35,6 @@ if [ -f "${BOX_DIRECTORY}/${VM_NAME}" ]; then
     exit 1
 fi
 
-sudo su - virtualbox
 sudo mv "${PATH_TO_VM}" "${BOX_DIRECTORY}/${VM_NAME}"
-vboxmanage registervm "${BOX_DIRECTORY}/${VM_NAME}/${VM_NAME}.vbox"
-./bin/start-vm.sh --wait "${VM_NAME}"
+${MANAGE_COMMAND} registervm "${BOX_DIRECTORY}/${VM_NAME}/${VM_NAME}.vbox"
+"${DIRECTORY}"/start-vm.sh --wait "${VM_NAME}"
