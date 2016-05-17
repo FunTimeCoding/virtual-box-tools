7cc33508c83a8cb486f8e8d2be8780eda6b2d4d0
Alexander Reitzel <funtimecoding@gmail.com>
2016-05-17 14:16:42 +0200

    fix create-new-machine script to actually create a new machine

--- bin/create-new-machine.sh
+++ bin/create-new-machine.sh
@@ -9,13 +9,22 @@ if [ "${MACHINE_NAME}" = "" ]; then
 fi
 
 # Create machine.
-vboxmanage createvm --name "${MACHINE_NAME}" --register
+# vboxmanage list ostypes
+vboxmanage createvm --name "${MACHINE_NAME}" --register --ostype Debian_64
 
-# Create disk.
+# Create a controller.
+CONTROLLER_NAME="SATA Controller"
+vboxmanage storagectl "${MACHINE_NAME}" --name "${CONTROLLER_NAME}" --add sata
+
+# Create a disk.
 DISK_NAME="${MACHINE_NAME}.vdi"
-vboxmanage createhd --filename "${DISK_NAME}" --size 16384
-vboxmanage storageattach "${MACHINE_NAME}" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${DISK_NAME}"
-vboxmanage storageattach "${MACHINE_NAME}" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium emptydrive
+MACHINE_DIRECTORY="${HOME}/VirtualBox VMs/${MACHINE_NAME}"
+DISK_PATH="${MACHINE_DIRECTORY}/${DISK_NAME}"
+vboxmanage createmedium disk --filename "${DISK_PATH}" --size 16384
+
+# Attach hard disk and DVD drive to the controller.
+vboxmanage storageattach "${MACHINE_NAME}" --storagectl "${CONTROLLER_NAME}" --port 0 --device 0 --type hdd --medium "${DISK_PATH}"
+vboxmanage storageattach "${MACHINE_NAME}" --storagectl "${CONTROLLER_NAME}" --port 1 --device 0 --type dvddrive --medium emptydrive
 
 # Modify settings.
 vboxmanage modifyvm "${MACHINE_NAME}" --acpi on
