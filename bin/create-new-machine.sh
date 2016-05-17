7284a81cd489fa656644b909e569232365b4f401
Alexander Reitzel <funtimecoding@gmail.com>
2016-05-17 15:58:40 +0200

    improve create-new-machine script to use TFTP

--- bin/create-new-machine.sh
+++ bin/create-new-machine.sh
@@ -1,41 +1,82 @@
 #!/bin/sh -e
 
-MACHINE_NAME="${1}"
+PRESEED_FILE="${1}"
+MACHINE_NAME="${2}"
 
-if [ "${MACHINE_NAME}" = "" ]; then
-    echo "Usage: ${0} MACHINE_NAME"
+if [ "${PRESEED_FILE}" = "" ] || [ "${MACHINE_NAME}" = "" ]; then
+    echo "Usage: ${0} PRESEED_FILE MACHINE_NAME"
 
     exit 1
 fi
 
-# Create machine.
-# vboxmanage list ostypes
+#vboxmanage list ostypes
 vboxmanage createvm --name "${MACHINE_NAME}" --register --ostype Debian_64
 
-# Create a controller.
 CONTROLLER_NAME="SATA Controller"
 vboxmanage storagectl "${MACHINE_NAME}" --name "${CONTROLLER_NAME}" --add sata
 
-# Create a disk.
 DISK_NAME="${MACHINE_NAME}.vdi"
 MACHINE_DIRECTORY="${HOME}/VirtualBox VMs/${MACHINE_NAME}"
 DISK_PATH="${MACHINE_DIRECTORY}/${DISK_NAME}"
 vboxmanage createmedium disk --filename "${DISK_PATH}" --size 16384
 
-# Attach hard disk and DVD drive to the controller.
 vboxmanage storageattach "${MACHINE_NAME}" --storagectl "${CONTROLLER_NAME}" --port 0 --device 0 --type hdd --medium "${DISK_PATH}"
 vboxmanage storageattach "${MACHINE_NAME}" --storagectl "${CONTROLLER_NAME}" --port 1 --device 0 --type dvddrive --medium emptydrive
 
-# Modify settings.
 vboxmanage modifyvm "${MACHINE_NAME}" --acpi on
-#vboxmanage modifyvm "${MACHINE_NAME}" --nic1 nat --nictype1 82540EM
-#vboxmanage modifyvm "${MACHINE_NAME}" --nattftpfile1 /debian.pxe
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
+    TFTP_DIRECTORY="${HOME}/.config/VirtualBox/TFTP"
+else
+    TFTP_DIRECTORY="${HOME}/Library/VirtualBox/TFTP"
+fi
+
+mkdir -p "${TFTP_DIRECTORY}/tmp"
+cp "${PRESEED_FILE}" "${TFTP_DIRECTORY}/preseed.cfg"
+cd "${TFTP_DIRECTORY}"
+
+if [ ! -f netboot.tar.gz ]; then
+    wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/netboot.tar.gz
+fi
+
+if [ ! -f version.info ]; then
+    tar xf netboot.tar.gz
+fi
+
+cd tmp
+
+if [ ! -d bin ]; then
+    gzip -d < ../debian-installer/amd64/initrd.gz | sudo cpio -i
+fi
+
+sudo cp ../preseed.cfg .
+
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
+    sudo chown root:root preseed.cfg
+else
+    sudo chown root:wheel preseed.cfg
+fi
+
+find . | cpio -o --format=newc | gzip -9c > ../initrd.gz
+cd ..
+cp initrd.gz debian-installer/amd64
+
+if [ ! -L debian.pxe ]; then
+    ln -s debian-installer/amd64/pxelinux.0 debian.pxe
+fi
+
+# Point NAT network at the TFTP file.
+vboxmanage modifyvm "${MACHINE_NAME}" --nattftpfile1 /debian.pxe
+
+echo "DEFAULT jessie
+LABEL jessie
+kernel debian-installer/amd64/linux
+append vga=normal initrd=debian-installer/amd64/initrd.gz auto=true priority=critical preseed/file=/preseed.cfg" >> debian-installer/amd64/boot-screens/syslinux.cfg
 
 # Boot from network for installation.
-#vboxmanage modifyvm "${MACHINE_NAME}" --boot1 net
+vboxmanage modifyvm "${MACHINE_NAME}" --boot1 net
 
-# Start setup.
+
 vboxmanage startvm "${MACHINE_NAME}" --type headless
-
-# Boot from disk first.
-#vboxmanage modifyvm "${MACHINE_NAME}" --boot1 disk
+vboxmanage modifyvm "${MACHINE_NAME}" --boot1 disk
--- bin/example.sh
+++ bin/example.sh
@@ -3,6 +3,7 @@
 echo "Only contains example vboxmanage commands."
 exit 1
 
+# CPU hot plug.
 # Enable CPU hot plugging. Machine must be stopped.
 vboxmanage modifyvm "${MACHINE_NAME}" --cpuhotplug on
 # Add a core. Machine can be running.
@@ -10,55 +11,23 @@ vboxmanage modifyvm "${MACHINE_NAME}" --plugcpu 1
 # Remove a core. Machine can be running.
 vboxmanage modifyvm "${MACHINE_NAME}" --unplugcpu 1
 
+
 # Resize a disk.
 vboxmanage modifyhd "${DISK_PATH}" --resize 32768
 
+
+# Set the network interface controller type.
 # Available nictype values: http://www.virtualbox.org/manual/ch06.html#nichardware
 vboxmanage modifyvm "${MACHINE_NAME}" --nic1 nat --nictype1 82540EM
 
-# Set address space.
+
+# Set address space of a NAT network.
 vboxmanage modifyvm "${MACHINE_NAME}" --natnet1 "192.168/16".
 
+
+# Forwards
 # Forward host port 2222 to guest port 22. FORWARD_NAME is an arbitrary string.
 FORWARD_NAME="example ssh"
 vboxmanage modifyvm "${MACHINE_NAME}" --natpf1 "${FORWARD_NAME},tcp,,2222,,22"
 # Remove a forward.
 vboxmanage modifyvm "${MACHINE_NAME}" --natpf1 delete "${FORWARD_NAME}"
-
-OPERATING_SYSTEM=$(uname)
-
-if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
-    TFTP_DIRECTORY="${HOME}/.config/VirtualBox/TFTP"
-else
-    TFTP_DIRECTORY="${HOME}/Library/VirtualBox/TFTP"
-fi
-
-# Use TFTP to automatically install a VM.
-mkdir -p "${TFTP_DIRECTORY}"
-cd "${TFTP_DIRECTORY}"
-wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/netboot.tar.gz
-tar xf netboot.tar.gz
-mkdir tmp
-cd tmp
-gzip -d < ../debian-installer/amd64/initrd.gz | sudo cpio -i
-# Use debian-tools to generate preseed.cfg.
-cd ~/Code/Personal/debian-tools
-PYTHONPATH=. bin/dt --hostname example --domain example.org --root-password root --user-name shiin --user-password shiin --user-real-name "Alexander Reitzel" --insecure > preseed.cfg
-sudo mv preseed.cfg "${TFTP_DIRECTORY}/tmp"
-popd
-sudo chown root:wheel preseed.cfg
-find . | cpio -o --format=newc | gzip -9c > ../initrd.gz
-cd ..
-cp initrd.gz debian-installer/amd64
-ln -s debian-installer/amd64/pxelinux.0 debian.pxe
-vboxmanage modifyvm "${MACHINE_NAME}" --nattftpfile1 /debian.pxe
-
-echo "DEFAULT jessie
-LABEL jessie
-kernel debian-installer/amd64/linux
-append vga=normal initrd=debian-installer/amd64/initrd.gz auto=true priority=critical preseed/file=/preseed.cfg" >> debian-installer/amd64/boot-screens/syslinux.cfg
-
-vboxmanage modifyvm "${MACHINE_NAME}" --boot1 net
-vboxmanage startvm "${MACHINE_NAME}" --type headless
-
-vboxmanage modifyvm "${MACHINE_NAME}" --boot1 disk
