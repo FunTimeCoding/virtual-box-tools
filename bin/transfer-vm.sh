1da835595fd764f770b440be179d14f600fa77b3
Alexander Reitzel <funtimecoding@gmail.com>
2016-09-17 16:21:48 +0200

    update transfer-vm script to work even if the machine isnt running and uses the sudo user from the config

--- bin/transfer-vm.sh
+++ bin/transfer-vm.sh
@@ -1,10 +1,20 @@
 #!/bin/sh -e
 
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Usage: ${0} MACHINE_NAME DESTINATION_HOST"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}"/../lib/virtual_box_tools.sh
 MACHINE_NAME="${1}"
 DESTINATION_HOST="${2}"
 
 if [ "${MACHINE_NAME}" = "" ] || [ "${DESTINATION_HOST}" = "" ]; then
-    echo "Usage: MACHINE_NAME DESTINATION_HOST"
+    usage
 
     exit 1
 fi
@@ -13,12 +23,27 @@ TEMPORARY_DIRECTORY="${HOME}/tmp/virtualbox"
 mkdir -p "${TEMPORARY_DIRECTORY}"
 
 if [ ! -d "${TEMPORARY_DIRECTORY}/${MACHINE_NAME}" ]; then
-    echo "Stop running machine"
-    bin/stop-vm.sh --wait "${MACHINE_NAME}"
-    echo "Make local copy of machine to minimize downtime"
-    sudo cp -R "/home/vbox/VirtualBox VMs/${MACHINE_NAME}" "${TEMPORARY_DIRECTORY}/${MACHINE_NAME}"
-    echo "Start machine again"
-    bin/start-vm.sh --wait "${MACHINE_NAME}"
+    STATE=$("${SCRIPT_DIRECTORY}"/get-vm-state.sh)
+
+    if [ "${STATE}" = running ]; then
+        echo "Stop running machine"
+        "${SCRIPT_DIRECTORY}"/bin/stop-vm.sh --wait "${MACHINE_NAME}"
+    fi
+
+    if [ "${SUDO_USER}" = "" ]; then
+        HOME_DIRECTORY="${HOME}"
+    else
+        HOME_DIRECTORY="/home/${SUDO_USER}"
+    fi
+
+    echo "Make local copy of machine"
+    sudo cp -R "${HOME_DIRECTORY}/VirtualBox VMs/${MACHINE_NAME}" "${TEMPORARY_DIRECTORY}/${MACHINE_NAME}"
+
+    if [ "${STATE}" = running ]; then
+        echo "Start machine again"
+        "${SCRIPT_DIRECTORY}"/bin/start-vm.sh --wait "${MACHINE_NAME}"
+    fi
+
     sudo chown -R shiin:shiin "${TEMPORARY_DIRECTORY}/${MACHINE_NAME}"
 fi
 
