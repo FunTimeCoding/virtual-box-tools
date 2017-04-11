cddb34abed1d295be3c786da4d8d6be37056f6a7
Alexander Reitzel <funtimecoding@gmail.com>
2017-04-11 18:06:00 +0200

    remove former input and scan-code scripts

--- bin/input.sh
+++ bin/input.sh
@@ -5,19 +5,22 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 
 usage()
 {
-    echo "Usage: ${0} MACHINE_NAME LINE"
+    echo "Usage: ${0} MACHINE_NAME TEXT"
 }
 
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}"/../lib/virtual_box_tools.sh
 MACHINE_NAME="${1}"
-LINE="${2}"
+TEXT="${2}"
 
-if [ "${MACHINE_NAME}" = "" ] || [ "${LINE}" = "" ]; then
+if [ "${MACHINE_NAME}" = "" ] || [ "${TEXT}" = "" ]; then
     usage
 
     exit 1
 fi
 
-SCAN_CODE=$("${SCRIPT_DIRECTORY}"/scan-code.py "${LINE}")
-${VBOXMANAGE} controlvm "${MACHINE_NAME}" keyboardputscancode ${SCAN_CODE}
+LINES=$("${SCRIPT_DIRECTORY}"/scan-code.py "${TEXT}")
+
+for LINE in ${LINES}; do
+    ${VBOXMANAGE} controlvm "${MACHINE_NAME}" keyboardputscancode ${LINE}
+done
