bc66cc26cb40099856b9a0b967990c113db02617
Alexander Reitzel <funtimecoding@gmail.com>
2017-04-11 18:00:37 +0200

    add alternative input script for alternative scan-code script

---
+++ bin/input2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
+
+usage()
+{
+    echo "Usage: ${0} MACHINE_NAME TEXT"
+}
+
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}"/../lib/virtual_box_tools.sh
+MACHINE_NAME="${1}"
+TEXT="${2}"
+
+if [ "${MACHINE_NAME}" = "" ] || [ "${TEXT}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+LINES=$("${SCRIPT_DIRECTORY}"/scan-code2.py "${TEXT}")
+
+for LINE in ${LINES}; do
+    ${VBOXMANAGE} controlvm "${MACHINE_NAME}" keyboardputscancode ${LINE}
+done
