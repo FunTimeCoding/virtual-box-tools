5b340d5a4b4d6ab948430b328804af925b7a48f5
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-10 21:07:55 +0200

    synchronize with skeleton

--- setup.sh
+++ setup.sh
@@ -1,9 +1,43 @@
 #!/bin/sh -e
 
+if [ "${1}" = --help ]; then
+    echo "Usage: ${0} [--no-venv]"
+fi
+
+USE_VENV=true
+
+if [ "${1}" = --no-venv ]; then
+    USE_VENV=false
+fi
+
 SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Linux ]; then
-    sudo apt-get --quiet 2 install libenchant-dev hunspell bc
+    dpkg --list | grep --quiet 'ii  libenchant-dev' && FOUND=true || FOUND=false
+
+    if [ "${FOUND}" = false ]; then
+        sudo apt-get --quiet 2 install libenchant-dev
+    fi
+
+    dpkg --list | grep --quiet 'ii  hunspell' && FOUND=true || FOUND=false
+
+    if [ "${FOUND}" = false ]; then
+        sudo apt-get --quiet 2 install hunspell
+    fi
+
+    dpkg --list | grep --quiet 'ii  bc' && FOUND=true || FOUND=false
+
+    if [ "${FOUND}" = false ]; then
+        sudo apt-get --quiet 2 install bc
+    fi
+fi
+
+if [ "${USE_VENV}" = true ]; then
+    if [ ! -d .venv ]; then
+        python3 -m venv .venv
+    fi
+
+    . .venv/bin/activate
 fi
 
 pip3 install --upgrade --requirement requirements.txt
