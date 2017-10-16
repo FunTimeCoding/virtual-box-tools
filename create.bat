7eb1bf50ebbfcbcc83a29840c739a20fbfcb61bf
Alexander Reitzel <funtimecoding@gmail.com>
2017-10-16 14:03:31 +0200

    fix comment syntax, add vagrant up command

--- create.bat
+++ create.bat
@@ -2,12 +2,15 @@
 
 if not exist tmp mkdir tmp
 
-set /p NAME="Username: "
-/* TODO: Make files with newlines. This would add a newline. echo %NAME% > tmp/user-name.txt */
-echo|set /p=%NAME% > tmp/user-name.txt
+set /p USER_NAME="User name: "
+:: TODO: Make files with newlines. This would add a newline.
+::echo %USER_NAME% > tmp/user-name.txt
+echo|set /p=%USER_NAME% > tmp/user-name.txt
 
 set /p FULL_NAME="Full name: "
 echo|set /p=%FULL_NAME% > tmp/full-name.txt
 
 set /p DOMAIN="Domain: "
 echo|set /p=%DOMAIN% > tmp/domain.txt
+
+vagrant up
