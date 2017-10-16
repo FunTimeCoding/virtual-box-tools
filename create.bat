ce8fb04428eb2183e43d1e29b6076ed7a6c29c19
Alexander Reitzel <funtimecoding@gmail.com>
2017-10-16 15:44:27 +0200

    fix spaces being appended after strings in create.bat

--- create.bat
+++ create.bat
@@ -2,19 +2,17 @@
 
 if not exist tmp mkdir tmp
 
-echo eth0 > tmp/ethernet-device.txt
+echo eth0> tmp/ethernet-device.txt
 copy /y nul "tmp/pypirc.txt"
 
 set /p USER_NAME="User name: "
-:: TODO: Make files with newlines. This would add a newline.
-::echo %USER_NAME% > tmp/user-name.txt
-echo|set /p=%USER_NAME% > tmp/user-name.txt
+echo %USER_NAME%> tmp/user-name.txt
 
 set /p FULL_NAME="Full name: "
-echo|set /p=%FULL_NAME% > tmp/full-name.txt
+echo %FULL_NAME%> tmp/full-name.txt
 
 set /p DOMAIN="Domain: "
-echo|set /p=%DOMAIN% > tmp/domain.txt
+echo %DOMAIN%> tmp/domain.txt
 
 vagrant up
 pause
