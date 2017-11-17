8b9812cbaee337dd3fb4ab2b37259826256ebbf9
Alexander Reitzel <funtimecoding@gmail.com>
2017-11-17 23:46:45 +0100

    remove unused code

--- bin/create-test-machine.sh
+++ bin/create-test-machine.sh
@@ -35,20 +35,13 @@ ${VBOXMANAGE} createmedium disk --filename tmp/example.vdi --size 4096
 ${VBOXMANAGE} storageattach example --storagectl "SATA controller" --port 0 --device 0 --type hdd --medium tmp/example.vdi
 ${VBOXMANAGE} storageattach example --storagectl "SATA controller" --port 1 --device 0 --type dvddrive --medium emptydrive
 ${VBOXMANAGE} modifyvm example --acpi on --cpus 1 --memory 1024 --vram 16
-SYSTEM=$(uname)
-
-if [ "${SYSTEM}" = Darwin ]; then
-    STAT=gstat
-    DATE=gdate
-else
-    STAT=stat
-    DATE=date
-fi
 
 # TODO: Creation time is not stored in the file system. Periodically delete it.
 if [ ! -f tmp/netboot.tar.gz ]; then
     wget --output-document tmp/netboot.tar.gz http://ftp.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/netboot.tar.gz
 fi
+
+SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Darwin ]; then
     DIRECTORY="${HOME}/Library/VirtualBox"
