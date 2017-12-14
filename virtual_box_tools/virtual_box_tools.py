86eb44b1578840df209bd5c8a0451fa4788c6fd4
Alexander Reitzel <funtimecoding@gmail.com>
2017-12-14 13:15:46 +0100

    return network adapter change to before installing additions

--- virtual_box_tools/virtual_box_tools.py
+++ virtual_box_tools/virtual_box_tools.py
@@ -583,6 +583,25 @@ class Commands:
             sudo_user=self.sudo_user
         )
 
+        if bridge_interface == '':
+            CommandProcess(
+                arguments=[
+                    'vboxmanage', 'modifyvm', name,
+                    '--nic1', 'hostonly',
+                    '--hostonlyadapter1', 'vboxnet0',
+                ],
+                sudo_user=self.sudo_user,
+            )
+        else:
+            CommandProcess(
+                arguments=[
+                    'vboxmanage', 'modifyvm', name,
+                    '--nic1', 'bridged',
+                    '--bridgeadapter1', bridge_interface,
+                ],
+                sudo_user=self.sudo_user,
+            )
+
         if not no_additions:
             self.start_host(name)
             sleep(60)
@@ -621,25 +640,6 @@ class Commands:
 
         server.shutdown()
         server.server_close()
-
-        if bridge_interface == '':
-            CommandProcess(
-                arguments=[
-                    'vboxmanage', 'modifyvm', name,
-                    '--nic1', 'hostonly',
-                    '--hostonlyadapter1', 'vboxnet0',
-                ],
-                sudo_user=self.sudo_user,
-            )
-        else:
-            CommandProcess(
-                arguments=[
-                    'vboxmanage', 'modifyvm', name,
-                    '--nic1', 'bridged',
-                    '--bridgeadapter1', bridge_interface,
-                ],
-                sudo_user=self.sudo_user,
-            )
 
     def keyboard_input(self, name: str, command: str) -> None:
         for line in ScanCode.scan(command).splitlines():
