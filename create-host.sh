432eddb5d2d0b9a2cc793d63edd4ef8121263dbf
Alexander Reitzel <funtimecoding@gmail.com>
2018-01-14 15:51:06 +0100

    add script to create host

---
+++ create-host.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+NAME="${1}"
+INTERFACE="${1}"
+
+if [ "${NAME}" = "" ] || [ "${INTERFACE}" = "" ]; then
+    echo "Usage: ${0} NAME INTERFACE"
+
+    exit 1
+fi
+
+vbt host create --name "${NAME}" --bridge-interface "${INTERFACE}"
+vbt host start --name "${NAME}"
+sleep 60
+vbt host show --name "${NAME}"
+vbt host stop --name "${NAME}"
+echo -ne '\007'
+
+while true; do
+    echo "Network configured? y/N"
+    read -r READ
+
+    if [ "${READ}" = y ]; then
+        break
+    fi
+done
+
+vbt host start --name "${NAME}"
+sleep 60
+bin/bootstrap-wrapper.sh "${NAME}"
+echo -ne '\007'
