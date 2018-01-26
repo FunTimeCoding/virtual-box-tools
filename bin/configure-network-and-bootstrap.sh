966672aaf1ff4068364fe510399b406f0050a8c2
Alexander Reitzel <funtimecoding@gmail.com>
2018-01-26 16:02:56 +0100

    uncomment remaining steps to do everything in one go

--- bin/configure-network-and-bootstrap.sh
+++ bin/configure-network-and-bootstrap.sh
@@ -14,20 +14,22 @@ if [ "${HOST_NAME}" = "" ] || [ "${NETMASK}" = "" ] || [ "${NETWORK}" = "" ] || 
     exit 1
 fi
 
-# TODO: Check if virtual machine is running.
-#vbt host start --name "${HOST_NAME}"
-#sleep 60
+STATE=$(vbt host show --name cm1 | grep --perl-regexp --only-matching "(?<=state: )[a-z]+")
+
+if [ "${STATE}" = stopped ]; then
+    vbt host start --name "${HOST_NAME}"
+    sleep 60
+    # TODO: Find a way to check if machine has finished booting with guest additions?
+fi
 
 USER_NAME=$(sqlite3 "${HOME}/.virtual-box-tools/user.sqlite" "SELECT user_name FROM user WHERE host_name = '${HOST_NAME}' AND user_name != 'root'")
 DOMAIN=$(sqlite3 "${HOME}/.virtual-box-tools/user.sqlite" "SELECT domain_name FROM user WHERE host_name = '${HOST_NAME}' AND user_name = 'root'")
 USER_PASSWORD=$(sqlite3 "${HOME}/.virtual-box-tools/user.sqlite" "SELECT password FROM user WHERE host_name = '${HOST_NAME}' AND user_name = '${USER_NAME}'")
 ROOT_PASSWORD=$(sqlite3 "${HOME}/.virtual-box-tools/user.sqlite" "SELECT password FROM user WHERE host_name = '${HOST_NAME}' AND user_name = 'root'")
 ADDRESS=$(dig +noall +answer "${HOST_NAME}.${DOMAIN}" | grep "${HOST_NAME}.${DOMAIN}" | awk '{ print $5 }')
-#bin/configure-network.sh "${HOST_NAME}" "${ROOT_PASSWORD}" "${ADDRESS}" "${NETMASK}" "${NETWORK}" "${BROADCAST}" "${GATEWAY}" "${NAMESERVER}" "${DOMAIN}"
-#vbt host stop --name "${HOST_NAME}"
-#sleep 30
-#vbt host start --name "${HOST_NAME}"
-#sleep 120
+bin/configure-network.sh "${HOST_NAME}" "${ROOT_PASSWORD}" "${ADDRESS}" "${NETMASK}" "${NETWORK}" "${BROADCAST}" "${GATEWAY}" "${NAMESERVER}" "${DOMAIN}"
+vbt host stop --name "${HOST_NAME}" --wait
+vbt host start --name "${HOST_NAME}" --wait
 bin/bootstrap-wrapper.sh "${HOST_NAME}" "${PUBLIC_KEY_PATH}"
 #PROXY=""
 #SALT_MASTER=""
