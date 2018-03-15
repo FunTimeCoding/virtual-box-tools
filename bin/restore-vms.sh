117f7198fd4395930fe9dfc23bad0732a22314f4
Alexander Reitzel <funtimecoding@gmail.com>
2018-03-15 15:27:43 +0100

    update stop and restore scripts to use vbt

--- bin/restore-vms.sh
+++ bin/restore-vms.sh
@@ -5,6 +5,6 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 LIST=$(cat "${SCRIPT_DIRECTORY}"/../running-vms.txt)
 
 for ELEMENT in ${LIST}; do
-    "${SCRIPT_DIRECTORY}"/start-vm.sh "${ELEMENT}"
+    vbt host start --name "${ELEMENT}"
     sleep 30
 done
--- bin/stop-running-vms.sh
+++ bin/stop-running-vms.sh
@@ -2,10 +2,9 @@
 
 DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
-FILE="${SCRIPT_DIRECTORY}"/../running-vms.txt
-"${SCRIPT_DIRECTORY}"/list-vms.sh > "${FILE}"
-LIST=$(cat "${SCRIPT_DIRECTORY}"/../running-vms.txt)
+LIST=$(vbt host list | jq --raw-output '.[].name')
+echo "${LIST}" > "${SCRIPT_DIRECTORY}"/../running-vms.txt
 
 for ELEMENT in ${LIST}; do
-    "${SCRIPT_DIRECTORY}"/stop-vm.sh "${ELEMENT}"
+    vbt host stop --name "${ELEMENT}" --wait
 done
