5ce9139b2f66aa72d3aaa2a547b5aff2073b97c3
Alexander Reitzel <funtimecoding@gmail.com>
2020-03-14 16:50:13 +0100

    catch and ignore exit code 2 in VBoxLinuxAdditions

--- virtual_box_tools/script/post-install.sh
+++ virtual_box_tools/script/post-install.sh
@@ -5,7 +5,17 @@ apt-get --quiet 2 update
 # dkms for additions, python-apt for Ansible to use check mode
 apt-get --quiet 2 install dkms python3-apt
 mount --options loop /dev/sr0 /mnt
-yes | sh /mnt/VBoxLinuxAdditions.run
+EXIT_CODE='0'
+REMOVE_INSTALLATION_DIR=0 yes | sh /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions || EXIT_CODE="${?}"
+
+if [ "${EXIT_CODE}" = '2' ]; then
+    echo "Ignore VBoxLinuxAdditions exit code 2."
+else
+    echo "VBoxLinuxAdditions exit code: ${EXIT_CODE}"
+
+    exit "${EXIT_CODE}"
+fi
+
 umount /mnt
 eject /dev/sr0
 apt-get --quiet 2 autoremove
